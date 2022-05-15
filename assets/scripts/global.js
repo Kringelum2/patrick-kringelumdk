5018d340b9d4f793352e613924ce5b487d307d2e
Kringelum2 <p.kringelum@gmail.com>
2022-05-15 19:23:37 +0200

    Frontpage design

---
+++ assets/scripts/global.js
@@ -0,0 +1,11 @@
+(function () {
+const currentLocation = location.href;
+const menuItem = document.getElementsByClassName('nav-item');
+const menuLength = menuItem.length
+for ( i = 0; i < menuLength; i++){
+if(menuItem[i].href === currentLocation){
+menuItem[i].className += " active"
+}
+}
+
+})();
