e726cd0e47cbf43a46ddde4dbbf4fe99a4d81937
Kringelum2 <p.kringelum@gmail.com>
2022-05-17 23:55:18 +0200

    Website DONE

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -1,11 +1,10 @@
 (function () {
-const currentLocation = location.href;
-const menuItem = document.getElementsByClassName('nav-item');
-const menuLength = menuItem.length
-for ( i = 0; i < menuLength; i++){
-if(menuItem[i].href === currentLocation){
-menuItem[i].className += " active"
-}
-}
-
+  const currentLocation = location.href;
+  const menuItem = document.getElementsByClassName("nav-item");
+  const menuLength = menuItem.length;
+  for (i = 0; i < menuLength; i++) {
+    if (menuItem[i].href === currentLocation) {
+      menuItem[i].className += " active";
+    }
+  }
 })();
